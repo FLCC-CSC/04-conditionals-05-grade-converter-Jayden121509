ddddc418d53fcdc7189abcbf29c4971670907abf
Jayden <257486807+Jayden121509@users.noreply.github.com>
2026-03-01 23:27:55 +0000

    grade converter

--- grade_converter.py
+++ grade_converter.py
@@ -1,9 +1,8 @@
 # FILE NAME - grade_converter.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
-
+# NAME: Jayden Blair
+# DATE: 3/1/26
+# BRIEF DESCRIPTION:  Grade converter that takes a grade from the user and converts it to a letter grade based on a grading scale.
 
 
 # 1. Make sure you fill out the comments above
@@ -15,22 +14,39 @@
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
-print('===== Grade Converter =====')
-
-percent = int(input('Enter a numerical grade (1-100): '))
-
-if percent > 100:
-    print('A+')
-elif percent >= 90:
-    print('A')
-elif percent >= 80:
-    print('B')
-elif percent >= 70:
-    print('C')
-elif percent >= 65:
-    print('D')
+print("===== Grade Converter =====")
+grade = int(input("Enter a numerical grade (1-100): "))
+if grade >= 97:
+    print("A+")
+elif grade >= 93:
+    print("A")
+elif grade >= 90:
+    print("A-")
+elif grade >= 87:
+    print("B+")
+elif grade >= 83:
+    print("B")
+elif grade >= 80:
+    print("B-")
+elif grade >= 77:
+    print("C+")
+elif grade >= 73:
+    print("C")
+elif grade >= 70:
+    print("C-")
+elif grade >= 67:
+    print("D+")
+elif grade >= 65:
+    print("D")
 else:
-    print('F')
+    print("F")
+
+
+
+
+
+
+
 
 ########### END YER CODE ABOVE THIS LINE ###########
 
@@ -85,8 +101,7 @@ D
 1. What is something you would tell a future student to be careful about when
    doing this lab?
 
-
-
+Not to forget to use elif statements when you have more than two options to choose from.
 
 
 
